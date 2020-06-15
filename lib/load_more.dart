4eda3e1b5cedf2da87069c99cc64d882f82f588a
Toan Nguyen V <toan.ngvan94@gmail.com>
2020-06-15 09:57:22 +0700

    add example

--- lib/load_more.dart
+++ lib/load_more.dart
@@ -1,7 +1,28 @@
 library load_more;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class Loadmore extends StatefulWidget {
+  final List datas;
+  final ListView child;
+
+  Loadmore({
+    @required this.datas,
+    @required this.child,
+  });
+  @override
+  _LoadmoreState createState() => _LoadmoreState();
+}
+
+class _LoadmoreState extends State<Loadmore> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
